65db64313b1167bde1092b34e185392e568522ea
Federico <ufinii@gmail.com>
2014-02-01 12:22:41 +0000

    Showing current song

--- scripts/current-song.js
+++ scripts/current-song.js
@@ -1,21 +1,31 @@
-require(['$api/models'], function(models) {
+require([
+  '$api/models',
+  '$views/image#Image'
+], function(models, Image) {
   var nowPlaying = document.getElementById('now-playing');
 
   function updateStatus(track) {
       if (track === null) {
-          nowPlaying.innerHTML = 'No track currently playing';
+        nowPlaying.innerHTML = 'No track currently playing';
       } else {
-          nowPlaying.innerHTML = 'Now playing: ' + track.name;
+        nowPlaying.innerHTML = 'Now playing: ' + track.name;
+        var album = track.album;
+        var image = Image.forAlbum(album, {width: 200, height: 200, player: true});
+        console.log("TEST");
+        console.log(image);
+        if (image != null && image.node != null) {
+          document.getElementById('now-playing-cover').appendChild(image.node);
+        }
       }
   }
 
   // update on load
   models.player.load('track').done(function(p) {
-      updateStatus(p.track);
+    updateStatus(p.track);
   });
 
   // update on change
   models.player.addEventListener('change', function(p) {
-      updateStatus(p.data.track);
+    updateStatus(p.data.track);
   });
 });
